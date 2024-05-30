a1efda9e8d6401c96bacbf96bdfafc26a3a5043d
mohammaduzer07 <134217616+mohammaduzer07@users.noreply.github.com>
2024-05-30 15:56:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,33 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
+
+
+function downloadimage(images) {
+	const promise = images.map((image)=>{
+		return new Promise((resolve, reject)=>{
+			let img = new Image();
+			img.src = image.url;
+			img.onload = ()=>{
+				resolve(img);
+			};
+			img.onerror = ()=>{
+				reject(`Failed to load image's URL: ${image.url}`)
+			};
+		});
+	});
+
+	Promise.all(promise)
+	.then((imgs) =>{
+		output.innerHTML = null;
+		imgs.forEach((img)=>{
+			output.appendChild(img);
+		});
+	})
+	.catch((error)=>{
+		console.log(error);
+	});
+}
+btn.addEventListener('click', ()=>{
+	downloadimage(images);
+});
